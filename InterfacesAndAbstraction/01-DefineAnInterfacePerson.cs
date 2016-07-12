9450617eb6dd9733a28ff063a54298b90c67d4ab
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-12 10:54:30 +0300

    Add files via upload

---
+++ InterfacesAndAbstraction/01-DefineAnInterfacePerson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+public interface IPerson
+{
+    string Name { get; }
+    int Age { get; }
+}
+
+public class Citizen : IPerson
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+
+    public Citizen(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
+
+public class DefineAnInterfacePerson
+{
+    public static void Main(string[] args)
+    {
+        Type personInterface = typeof(Citizen).GetInterface("IPerson");
+        PropertyInfo[] properties = personInterface.GetProperties();
+        Console.WriteLine(properties.Length);
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        IPerson person = new Citizen(name, age);
+        Console.WriteLine(person.Name);
+        Console.WriteLine(person.Age);
+    }
+}
