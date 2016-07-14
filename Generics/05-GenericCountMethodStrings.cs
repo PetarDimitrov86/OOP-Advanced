fadf398dd4e070d6d5ccc8e92ba1444d3e292c29
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-14 09:17:03 +0300

    probably not the right way to do it

---
+++ Generics/05-GenericCountMethodStrings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GenericBox<T>
+{
+    private T type;
+    public static int Counter;
+
+    public GenericBox(T type)
+    {
+        this.type = type;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.type.GetType().FullName}: {this.type}";
+    }
+
+    public static void Compare<T>(List<T> list, T element)
+        where T : IComparable<T>
+    {
+        foreach (var generic in list)
+        {
+            if (generic.CompareTo(element)> 0)
+            {
+                Counter++;
+            }
+        }
+    }
+}
+
+class GenericCountMethodStrings
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<string> genericList = new List<string>();
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            genericList.Add(input);
+        }
+        string comparingElement = Console.ReadLine();
+        GenericBox<string>.Compare<string>(genericList, comparingElement);
+        Console.WriteLine(GenericBox<string>.Counter);
+    }
+}
