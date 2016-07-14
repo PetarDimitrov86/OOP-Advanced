20fb6129371d7c0a11c0401dc04921e48af3cfa5
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-14 10:38:20 +0300

    Add files via upload

---
+++ Generics/10-Pr10Tuple.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Tuple<K, V>
+{
+    private readonly K key;
+    private readonly V value;
+
+    public Tuple(K key, V value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.key} -> {this.value}";
+    }
+}
+
+public class Pr10Tuple
+{
+    static void Main(string[] args)
+    {
+        string[] nameAndAdress = Console.ReadLine().Split();
+        string fullName = nameAndAdress[0] + " " + nameAndAdress[1];
+        string address = nameAndAdress[2];
+        Tuple<string, string> firstTuple = new Tuple<string, string>(fullName, address);
+
+        string[] personAndBeer = Console.ReadLine().Split();
+        string name = personAndBeer[0];
+        int beer = int.Parse(personAndBeer[1]);
+        Tuple<string, int> secondTuple = new Tuple<string, int>(name, beer);
+
+        string[] integerAndDouble = Console.ReadLine().Split();
+        int firstNum = int.Parse(integerAndDouble[0]);
+        double secondNum = double.Parse(integerAndDouble[1]);
+        Tuple<int, double> thirdTuple = new Tuple<int, double>(firstNum, secondNum);
+
+        Console.WriteLine(firstTuple);
+        Console.WriteLine(secondTuple);
+        Console.WriteLine(thirdTuple);
+    }
+}
