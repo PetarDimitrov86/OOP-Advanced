ed9148b0f334bf3a7de8c793d52eebe3f08b4e3a
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-14 09:24:23 +0300

    Add files via upload

---
+++ Generics/06-GenericCountMethodDoubles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class GenericCountMethodDoubles
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<double> doubleList = new List<double>();
+        for (int i = 0; i < n; i++)
+        {
+            double number = double.Parse(Console.ReadLine());
+            doubleList.Add(number);
+        }
+        double comparingElement = double.Parse(Console.ReadLine());
+        Console.WriteLine(Compare(doubleList, comparingElement));
+    }
+
+    public static int Compare<T>(List<T> list, T element)
+        where T : IComparable<T>
+    {
+        int counter = 0;
+        foreach (var generic in list)
+        {
+            if (generic.CompareTo(element) > 0)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
