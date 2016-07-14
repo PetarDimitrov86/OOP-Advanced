5d35faa48f71921d9c0c5856317c0204c93e8b1a
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-14 07:57:54 +0300

    Add files via upload

---
+++ Generics/02-GenericBoxOfInteger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class GenericBox<T>
+{
+    private T type;
+
+    public GenericBox(T type)
+    {
+        this.type = type;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.type.GetType().FullName}: {this.type}";
+    }
+}
+
+public class GenericBoxOfInteger
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            GenericBox<int> genericInt = new GenericBox<int>(number);
+            Console.WriteLine(genericInt);
+        }
+    }
+}
