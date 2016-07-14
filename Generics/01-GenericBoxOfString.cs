c0e41b594aa7c26e2401f08bf1e185e4b39be7cd
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-14 07:56:17 +0300

    Add files via upload

---
+++ Generics/01-GenericBoxOfString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class GenericBox<T>
+{
+    private string name;
+
+    public GenericBox(string name)
+    {
+        this.name = name;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.name.GetType().FullName}: {this.name}";
+    }
+}
+
+public class GenericBoxOfString
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            GenericBox<string> generic = new GenericBox<string>(input);
+            Console.WriteLine(generic);
+        }
+    }
+}
