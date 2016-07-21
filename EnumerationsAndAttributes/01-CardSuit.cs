415ace16951a630c88dd195a34a40c91092bc76b
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-21 12:25:28 +0300

    Add files via upload

---
+++ EnumerationsAndAttributes/01-CardSuit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public enum Suits
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
+
+public class CardSuit
+{
+    static void Main()
+    {
+        string enumToDisplay = Console.ReadLine();
+        var cardTypes = Enum.GetValues(typeof(Suits));
+        Console.WriteLine(enumToDisplay + ":");
+        foreach (var cardType in cardTypes)
+        {
+            Console.WriteLine($"Ordinal value: {(int)cardType}; Name value: {cardType}");
+        }
+    }
+}
