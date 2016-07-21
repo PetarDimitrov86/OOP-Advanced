b08fe2c604329fd85b19c863d6882b9ff604b2e6
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-21 12:28:38 +0300

    Add files via upload

---
+++ EnumerationsAndAttributes/02-CardRank.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public enum Ranks
+{
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
+
+public class CardRank
+{
+    static void Main(string[] args)
+    {
+        string enumToDisplay = Console.ReadLine();
+        var cardTypes = Enum.GetValues(typeof(Ranks));
+        Console.WriteLine(enumToDisplay + ":");
+        foreach (var cardType in cardTypes)
+        {
+            Console.WriteLine($"Ordinal value: {(int)cardType}; Name value: {cardType}");
+        }
+    }
+}
