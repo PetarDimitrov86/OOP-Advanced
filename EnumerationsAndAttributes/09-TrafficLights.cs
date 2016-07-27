8ad2436832fbf1d9862ea023695e1f11cf4a7769
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-27 10:46:45 +0300

    Add files via upload

---
+++ EnumerationsAndAttributes/09-TrafficLights.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public enum Light
+{
+    Red,
+    Green,
+    Yellow
+}
+
+public class TrafficLights
+{
+    static void Main()
+    {
+        string[] lights = Console.ReadLine().Split();
+        int n = int.Parse(Console.ReadLine());
+        int itterator = 0;
+        for (int i = 0; i < n; i++)
+        {
+            foreach (var light in lights)
+            {
+                int nextLightIndex = (int) Enum.Parse(typeof(Light), light) + 1 + itterator;
+                Light nextLight = (Light) (nextLightIndex % 3);
+                Console.Write(nextLight + " "); 
+            }
+            Console.WriteLine();
+            itterator++;
+        }
+    }
+}
