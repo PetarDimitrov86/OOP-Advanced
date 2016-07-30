1b32ad8e863f65754ac3d4a2215e69e27af4c67c
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-30 18:10:47 +0300

    Add files via upload

---
+++ ObjectCommunicationAndEvents/02-KingsGambit.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public interface IMoodChangeable
+{
+    string Name { get; set; }
+
+    void OnKingAttack(object sender, EventArgs args);
+}
+
+public abstract class Person
+{
+    public string Name { get; set; }
+
+    protected Person(string name)
+    {
+        Name = name;
+    }
+}
+
+public delegate void KingAttackHandler(object sender, EventArgs e);
+
+public class King : Person
+{
+    public event KingAttackHandler Attacked;
+
+    public King(string name) : base(name)
+    {
+    }
+
+    public void OnAttack()
+    {
+        Console.WriteLine($"King {this.Name} is under attack!");
+        this.Attacked?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+public class Footman : Person, IMoodChangeable
+{
+    public Footman(string name) : base(name)
+    {
+    }
+
+    public void OnKingAttack(object sender, EventArgs args)
+    {
+        Console.WriteLine($"Footman {this.Name} is panicking!");
+    }
+}
+
+public class RoyalGuard : Person, IMoodChangeable
+{
+    public RoyalGuard(string name) : base(name)
+    {
+    }
+
+    public void OnKingAttack(object sender, EventArgs args)
+    {
+        Console.WriteLine($"Royal Guard {this.Name} is defending!");
+    }
+}
+
+public class KingsGambit
+{
+    static void Main()
+    {
+        List<IMoodChangeable> soldiersList = new List<IMoodChangeable>();
+
+        King king = new King(Console.ReadLine());
+
+        string[] royalGuards = Console.ReadLine().Split();
+        for (int i = 0; i < royalGuards.Length; i++)
+        {
+            var guard = new RoyalGuard(royalGuards[i]);
+            king.Attacked += guard.OnKingAttack;
+            soldiersList.Add(guard);
+        }
+
+        string[] footmen = Console.ReadLine().Split();
+        for (int i = 0; i < footmen.Length; i++)
+        {
+            var footman = new Footman(footmen[i]);
+            king.Attacked += footman.OnKingAttack;
+            soldiersList.Add(footman);
+        }
+
+        string command = Console.ReadLine();
+        while (command != "End")
+        {
+            string[] commandArgs = command.Split();
+            switch (commandArgs[0])
+            {
+                case "Attack":
+                    king.OnAttack();
+                    break;
+                case "Kill":
+                    IMoodChangeable currentSoldier = soldiersList.FirstOrDefault(s => s.Name == commandArgs[1]);
+                    king.Attacked -= currentSoldier.OnKingAttack;
+                    soldiersList.Remove(currentSoldier);
+                    break;
+            }
+            command = Console.ReadLine();
+        }
+    }
+}
