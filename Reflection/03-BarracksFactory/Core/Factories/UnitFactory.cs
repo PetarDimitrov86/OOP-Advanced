2e213298ae62890cc1d6aeb6a3a5907df2a36fdc
Petar Dimitrov <lbmikesmith@gmail.com>
2016-07-30 19:26:40 +0300

    Add files via upload

---
+++ Reflection/03-BarracksFactory/Core/Factories/UnitFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace _03BarracksFactory.Core.Factories
+{
+    using System;
+    using Contracts;
+
+    public class UnitFactory : IUnitFactory
+    {
+        public IUnit CreateUnit(string unitType)
+        {
+            Type T = Assembly.GetExecutingAssembly().DefinedTypes.First(t => t.Name == unitType);
+            IUnit unit = Activator.CreateInstance(T) as IUnit;
+            return unit;
+        }
+    }
+}
---
+++ Reflection/03-BarracksFactory/Models/Units/Horseman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03BarracksFactory.Models.Units
+{
+    public class Horseman : Unit
+    {
+        private const int DefaultHealth = 50;
+        private const int DefaultDamage = 10;
+
+        public Horseman() : base(DefaultHealth, DefaultDamage)
+        {
+        }
+    }
+}
